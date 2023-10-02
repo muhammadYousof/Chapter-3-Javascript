5c8f225bfa54359035411b9b6e6a1f0c86bcd8af
muhammadYousof <my 7370044@gmail.com>
2023-10-02 12:59:05 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var age = "i am 20 years old"
+
+alert(age)
+
+var info = "You have Visited this Site 14 times"
+
+alert(info)
+
+var doc1 = "My Birth year is 2002"
+var doc2 = "Data type of My ddeslared varibal is number"
+
+document.write(doc1 +"<br/>"+ doc2)
+
+
+
+
+document.write("<h1>jhon Doe Oedered 5 T-Shirt(S) on XYZ Clothing Store</h1>")
+document.write("<h3></h3>")
+
+var visitorsname = "jhon doe"
+var producttitle = "Tshirts"
+
+// document.write(visitorsname)
